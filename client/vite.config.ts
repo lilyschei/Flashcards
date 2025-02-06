d9f344f685a7440887c2a0e9c27641c393628362
lilyschei <41328366+lilyschei@users.noreply.github.com>
2025-02-06 17:14:38 +1100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
